645210cbbdb46baf73272d377c8137b6ba1961a1
moisesrmz <arturormz00@gmail.com>
2025-01-15 15:32:13 -0600

    re-calculo de tiempo de inactividad

--- app.py
+++ app.py
@@ -93,11 +93,12 @@ def calculate_oee(line, ideal_cycle_time=10):
     adjusted_elapsed_time = max(0, elapsed_time - proportional_break_time)
     line_label = folder_labels.get(line, line)
     inactive_time = pulse_history_data["inactiveTimeByLine"].get(line_label, 0)
-    operational_time = max(0, adjusted_elapsed_time - inactive_time)
+    adjusted_inactive_time = max(0, inactive_time - proportional_break_time)
+    operational_time = max(0, adjusted_elapsed_time - adjusted_inactive_time)
     good_pieces = pass_fail_counts[line].get("Passed", 0)
     total_pieces = pass_fail_counts[line].get("Passed", 0) + pass_fail_counts[line].get("Failed", 0)
     if adjusted_elapsed_time > 0:
-        availability = (adjusted_elapsed_time - inactive_time) / adjusted_elapsed_time
+        availability = (adjusted_elapsed_time - adjusted_inactive_time) / adjusted_elapsed_time
     else:
         availability = 0
     if good_pieces == 0 or operational_time == 0:
@@ -116,7 +117,7 @@ def calculate_oee(line, ideal_cycle_time=10):
     print(f"[INFO] Tiempo de Comedor Proporcional: {proportional_break_time:.2f}s")
     #print(f"[INFO] Tiempo Ajustado: {adjusted_elapsed_time:.2f}s")
     print(f"[INFO] Producción Real: {good_pieces}")
-    print(f"[INFO] Tiempo Inactivo: {inactive_time:.2f}s")
+    print(f"[INFO] Tiempo Inactivo: {inactive_time:.2f}s y ajustado: {adjusted_inactive_time:.2f}s")
     print(f"[INFO] Tiempo Operativo: {operational_time:.2f}s")
     print(f"[INFO] Performance: {performance:.2%}")
     #print(f"[INFO] Piezas Totales: {total_pieces}")
@@ -131,7 +132,7 @@ def calculate_oee(line, ideal_cycle_time=10):
         "elapsed_time": elapsed_time,
         "proportional_break_time": proportional_break_time,
         "adjusted_elapsed_time": adjusted_elapsed_time,
-        "inactive_time": inactive_time,
+        "inactive_time": adjusted_inactive_time,
         "operational_time": operational_time,
         "good_pieces": good_pieces,
         "total_pieces": total_pieces,
