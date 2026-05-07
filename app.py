5aa8dc214921cc37dcc954f1d6f5dde7bb821f3b
Moises Ramirez <arturormz00@gmail.com>
2026-05-07 09:08:02 -0600

    acomodo de lineas import

--- app.py
+++ app.py
@@ -19,8 +19,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-import shutil
 import csv
+from flask import send_file, make_response
+import io
+import csv
+
 
 
 app = Flask(__name__)
